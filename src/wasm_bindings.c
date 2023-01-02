5d65dccb20f020ad448006223ba4673cac3d4c8a
zach <zach@dylib.so>
2023-01-02 14:56:17 -0800

    cleanup: bindings

--- src/wasm_bindings.c
+++ src/wasm_bindings.c
@@ -14,30 +14,32 @@ value Val_some(value v) {
   CAMLparam1(v);
   CAMLlocal1(some);
   some = caml_alloc_small(1, 0);
-  Field(some, 0) = v;
+  Store_field(some, 0, v);
   CAMLreturn(some);
 }
 
 value wasm_verify_string(value s) {
   CAMLparam1(s);
   CAMLlocal2(e, x);
+  x = Val_unit;
   char *err = wasmstore_verify_string(String_val(s), caml_string_length(s));
   if (err != NULL) {
     e = caml_copy_string(err);
     wasmstore_error_free(err);
-    CAMLreturn(Val_some(e));
+    x = Val_some(e);
   }
-  CAMLreturn(Val_unit);
+  CAMLreturn(x);
 }
 
 value wasm_verify_file(value s) {
   CAMLparam1(s);
   CAMLlocal2(e, x);
+  x = Val_unit;
   char *err = wasmstore_verify_file(String_val(s));
   if (err != NULL) {
     e = caml_copy_string(err);
     wasmstore_error_free(err);
-    CAMLreturn(Val_some(e));
+    x = Val_some(e);
   }
-  CAMLreturn(Val_unit);
+  CAMLreturn(x);
 }
