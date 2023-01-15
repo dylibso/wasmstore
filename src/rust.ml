c5f30b74eae7bd4234bab230cca753243100cac2
zach <zachshipko@gmail.com>
2023-01-14 19:09:32 -0800

    format, add conf-rust-2021

--- src/rust.ml
+++ src/rust.ml
@@ -4,5 +4,5 @@ open! Bigarray
 
 (* file: lib.rs *)
 
-external wasm_verify_file: string -> string option = "wasm_verify_file"
-external wasm_verify_string: string -> string option = "wasm_verify_string"
+external wasm_verify_file : string -> string option = "wasm_verify_file"
+external wasm_verify_string : string -> string option = "wasm_verify_string"
