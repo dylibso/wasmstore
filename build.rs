919e49bf1a8b824214d04d8ee3666b706e06cd1c
zach <zach@dylibso.com>
2023-07-18 17:37:01 -0700

    cleanup: run dune fmt after building rust bindings

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
 pub fn main() -> std::io::Result<()> {
-    ocaml_build::Sigs::new("src/rust.ml").generate()
+    ocaml_build::Sigs::new("src/rust.ml").generate()?;
+    let _ = std::process::Command::new("dune")
+        .args(&["build", "@fmt", "--auto-promote"])
+        .status();
+    Ok(())
 }
