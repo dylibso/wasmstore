3a214bbfc23a471294c30d81a089765fc466b893
zach <zach@dylibso.com>
2023-07-18 21:00:22 -0700

    fix: use ocaml_string type

--- src/rust.ml
+++ src/rust.ml
@@ -7,10 +7,10 @@ external _wasm_error_free : unit -> unit = "wasm_error_free"
 let fn = Foreign.foreign
 
 let wasm_verify_file =
-  fn "wasm_verify_file" (string @-> size_t @-> returning (ptr char))
+  fn "wasm_verify_file" (ocaml_string @-> size_t @-> returning (ptr char))
 
 let wasm_verify_string =
-  fn "wasm_verify_string" (string @-> size_t @-> returning (ptr char))
+  fn "wasm_verify_string" (ocaml_string @-> size_t @-> returning (ptr char))
 
 let wasm_error_free = fn "wasm_error_free" (ptr char @-> returning void)
 let clone_string s = Bytes.unsafe_of_string s |> Bytes.to_string
@@ -25,8 +25,8 @@ let wrap res =
 
 let verify_file filename : (unit, [ `Msg of string ]) result =
   let len = Unsigned.Size_t.of_int (String.length filename) in
-  wrap @@ wasm_verify_file filename len
+  wrap @@ wasm_verify_file (ocaml_string_start filename) len
 
 let verify_string str : (unit, [ `Msg of string ]) result =
   let len = Unsigned.Size_t.of_int (String.length str) in
-  wrap @@ wasm_verify_string str len
+  wrap @@ wasm_verify_string (ocaml_string_start str) len
